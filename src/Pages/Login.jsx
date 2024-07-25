d8a1aa02a9de8f943552de66281bb91a7c6b32ae
parthamodak <mm.partho@gmail.com>
2024-07-25 16:20:01 +0600

    second video

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import SignupImage from "../assets/Signup.jpeg";
+import { Link } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Login = () => {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  let [emailerror, setEmailerror] = useState("");
+  let [passworderror, setPassworderror] = useState("");
+  let [passwordShow, setPasswordShow] = useState(false);
+
+  let handleEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailerror("");
+  };
+
+  let handlePassword = (e) => {
+    setPassword(e.target.value);
+    setPassworderror("");
+  };
+
+  let handleSubmit = () => {
+    if (!email) {
+      setEmailerror("Email is required");
+    } else if (!/^\w+([\.-]?\w+)*@w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setEmailerror("Invalid Email");
+    }
+
+    if (!password) {
+      setPassworderror("Password is required");
+    }
+  };
+
+  // let handlePasswordShow = () => {};
+
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-2/4 h-full flex justify-end items-center">
+        <div className="mr-[69px]">
+          <h1 className="text-[34px] font-bold text-secondary">
+            Login To Your Account
+          </h1>
+
+          <div className="w-[368px] h-[80px] mt-[61px] relative">
+            <label
+              className={`text-sm font-semibold ${
+                emailerror ? "text-red-500" : "text-secondary"
+              } absolute top-[-9px] left-[50px] bg-white px-2`}
+            >
+              Email Adress
+            </label>
+            <input
+              onChange={handleEmail}
+              className={`w-full h-full border-b ${
+                emailerror ? "border-red-500" : "border-secondary/50"
+              } rounded-2xl pl-[50px] `}
+              type="email"
+              value={email}
+              placeholder=" Enter your Email Adress"
+            />
+            {emailerror && (
+              <p className=" text-red-500 text-xm font-nunito">{emailerror}</p>
+            )}
+          </div>
+
+          <div className="w-[368px] h-[80px] mt-[61px] relative">
+            <label
+              className={`text-sm font-semibold ${
+                passworderror ? "text-red-500" : "text-secondary"
+              } absolute top-[-9px] left-[50px] bg-white px-2`}
+            >
+              {""} Password
+            </label>
+            <input
+              onChange={handlePassword}
+              className={`w-full h-full border-b ${
+                passworderror ? "border-red-500" : "border-secondary/50"
+              } rounded-2xl pl-[50px]`}
+              type={passwordShow ? "text" : "password"}
+              value={password}
+              placeholder="Enter your Password"
+            />
+            {passwordShow ? (
+              <FaEye
+                onClick={() => setPasswordShow(false)}
+                className=" text-2xl absolute top-2/4 translate-y-[-50%] right-5 cursor-pointer"
+              />
+            ) : (
+              <FaEyeSlash
+                onClick={() => setPasswordShow(true)}
+                className=" text-2xl absolute top-2/4 translate-y-[-50%] right-5 cursor-pointer"
+              />
+            )}
+
+            {passworderror && (
+              <p className=" text-red-500 text-xm font-nunito">
+                {passworderror}
+              </p>
+            )}
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="bg-primary w-[368px] py-5 text-xl font-semibold text-white rounded-[86px] mt-[51px] "
+          >
+            Sign In
+          </button>
+
+          <p className="text-sm text-secondary text-center w-[368px] mt-[35px]">
+            Do not have any account?{""}
+            <Link to="/signup" className="text-[#EA6C00] font-bold ">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="w-2/4 h-full ">
+        <img
+          className="ml-auto h-full object-cover"
+          src={SignupImage}
+          alt="SignupImage"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import SignupImage from "../assets/Signup.jpeg";
+import { IoIosEye, IoIosEyeOff } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  let [email, setEmail] = useState("");
+  let [name, setName] = useState("");
+  let [pass, setPass] = useState("");
+  let [emailerr, setEmailerr] = useState("");
+  let [nameerr, setNameerr] = useState("");
+  let [passerr, setPasserr] = useState("");
+  let [passwordshow, setPasswordshow] = useState(false);
+
+  let handlemail = (e) => {
+    setEmail(e.target.value);
+    setEmailerr("");
+  };
+
+  let handlename = (e) => {
+    setName(e.target.value);
+    setNameerr("");
+  };
+  let handlepass = (e) => {
+    setPass(e.target.value);
+    setPasserr("");
+  };
+
+  let handleSubmit = () => {
+    if (!email) {
+      setEmailerr("Email is required");
+    } else if (!/^\w+([\.-]?\w+)*@w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setEmailerr("Invalid Email");
+    }
+
+    if (!name) {
+      setNameerr("Name is required");
+    }
+    if (!pass) {
+      setPasserr("Password is required");
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex  h-screen font-nunito">
+        <div className="w-1/2 h-full relative">
+          <div className="h-full flex flex-col justify-center absolute left-[50%] translate-x-[-50%]">
+            <h1 className="text-[30px] font-bold text-secondary mb-3 max-w-[435px]">
+              Get started with easily register
+            </h1>
+            <p className="mb-[30px] text-[20px]">
+              Free register and you can enjoy it
+            </p>
+            <div className="relative mt-4">
+              <input
+                onChange={handlemail}
+                className={`pl-8 w-[365px] h-[50px] border-[1px] ${
+                  emailerr ? "border-red-500" : " border-secondary"
+                }  rounded-lg`}
+                type="text"
+                placeholder="Enter you email"
+                value={email}
+              />
+              {emailerr && <p className="text-red-700">{emailerr}</p>}
+
+              <label
+                className={`absolute top-[-9px] left-5 ${
+                  emailerr ? "text-red-500" : "text-secondary"
+                } bg-white px-3 text-[13px]`}
+              >
+                Email Adress
+              </label>
+            </div>
+            <div className="relative mt-10">
+              <input
+                onChange={handlename}
+                className={`pl-8 w-[365px] h-[50px] border-[1px] ${
+                  nameerr ? "border-red-500" : " border-secondary"
+                }  rounded-lg`}
+                type="text"
+                placeholder="Enter you name"
+              />
+              {nameerr && <p className="text-red-700">{nameerr}</p>}
+              <p
+                className={`absolute top-[-9px] left-5 ${
+                  nameerr ? "text-red-500" : "text-secondary"
+                } bg-white px-3 text-[13px]`}
+              >
+                Full Name
+              </p>
+            </div>
+            <div className="relative mt-10">
+              {passwordshow ? (
+                <IoIosEye
+                  onClick={() => setPasswordshow(false)}
+                  className=" absolute top-2/4 translate-y-[-50%] right-5 text-2xl cursor-pointer"
+                />
+              ) : (
+                <IoIosEyeOff
+                  onClick={() => setPasswordshow(true)}
+                  className=" absolute top-2/4 translate-y-[-50%] right-5 text-2xl cursor-pointer"
+                />
+              )}
+
+              <input
+                onChange={handlepass}
+                className={`pl-8 w-[365px] h-[50px] border-[1px] ${
+                  passerr ? "border-red-500" : " border-secondary"
+                }  rounded-lg`}
+                type={passwordshow ? "text" : "password"}
+                placeholder="Enter your password"
+              />
+
+              {passerr && <p className="text-red-700">{passerr}</p>}
+              <p
+                className={`absolute top-[-9px] left-5 ${
+                  passerr ? "text-red-500" : "text-secondary"
+                } bg-white px-3 text-[13px]`}
+              >
+                Password
+              </p>
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="text-5 text-white font-semibold w-[365px] bg-primary py-3 mt-5 rounded-lg"
+            >
+              Sign Up
+            </button>
+            <p className="text-center mt-5 ">
+              Already Have an Account ?
+              <Link
+                to="./Signin"
+                className="text-yellow-600 font-bold cursor-pointer ml-1"
+              >
+                login
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className=" w-1/2 h-full">
+          <img
+            className="h-full w-full object-cover"
+            src={SignupImage}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
